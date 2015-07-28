8d8046bd3a93ad889e40f40fb9c8190c2a44ec5d
Aldo Mendez <aldomendez86@gmail.com>
2015-07-27 22:51:35 -0500

    aun no funciona el selector de codigo

--- tagManager.js
+++ tagManager.js
@@ -1,10 +1,13 @@
-Tasks = new Mongo.Collection("tags");
- 
+Tags = new Mongo.Collection("tags");
+Codes = new Mongo.Collection("codes");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tags: function () {
+      return Tags.find();
+    },
+    codes: function () {
+      return Codes.find();
     }
   });
 }
